6b13f18c674edc5ee6052c8825a3c79225d96d6c
Muhammad Asyraf <acapxasyrafx@yahoo.com>
2021-03-17 17:07:45 +0800

    base

---
+++ src/services/themes.js
@@ -0,0 +1,125 @@
+export const originalTheme = Object.freeze({
+  context: {
+    'gradient': true,
+    'shadow': 'lg', // 3 states: 'sm', 'lg', undefined (no shadow).
+    'invertedColor': false,
+  },
+  colors: {
+    primary: '#3a3838',
+    secondary: '#FFABAB',
+    success: '#40e583',
+    info: '#2c82e0',
+    danger: '#e34b4a',
+    warning: '#ffc200',
+    gray: '3a3838',
+    dark: '#34495e',
+  },
+})
+
+// export const originalTheme = Object.freeze({
+//   context: {
+//     'gradient': true,
+//     'shadow': 'lg', // 3 states: 'sm', 'lg', undefined (no shadow).
+//     'invertedColor': false,
+//   },
+//   colors: {
+//     primary: '#e34b4a',
+//     secondary: '#48dbd6',
+//     success: '#40e583',
+//     info: '#2c82e0',
+//     danger: '#e34b4a',
+//     warning: '#ffc200',
+//     gray: '3a3838',
+//     dark: '#34495e',
+//   },
+// })
+
+// export const originalTheme = Object.freeze({
+//   context: {
+//     'gradient': true,
+//     'shadow': 'lg', // 3 states: 'sm', 'lg', undefined (no shadow).
+//     'invertedColor': false,
+//   },
+//   colors: {
+//     primary: '#c33030',
+//     secondary: '#babfc2',
+//     success: '#40e583',
+//     info: '#2c82e0',
+//     danger: '#e34b4a',
+//     warning: '#ffc200',
+//     gray: '3a3838',
+//     dark: '#34495e',
+//   },
+// })
+
+// export const originalTheme = Object.freeze({
+//   context: {
+//     'gradient': true,
+//     'shadow': 'lg', // 3 states: 'sm', 'lg', undefined (no shadow).
+//     'invertedColor': false,
+//   },
+//   colors: {
+//     primary: '#ffffff',
+//     secondary: '#2c82e0',
+//     success: '#40e583',
+//     info: '#2c82e0',
+//     danger: '#e34b4a',
+//     warning: '#ffc200',
+//     gray: '3a3838',
+//     dark: '#34495e',
+//   },
+// })
+
+// export const originalTheme = Object.freeze({
+//   context: {
+//     'gradient': true,
+//     'shadow': 'lg', // 3 states: 'sm', 'lg', undefined (no shadow).
+//     'invertedColor': false,
+//   },
+//   colors: {
+//     primary: '#000000',
+//     secondary: '#ffffff',
+//     success: '#40e583',
+//     info: '#2c82e0',
+//     danger: '#e34b4a',
+//     warning: '#ffc200',
+//     gray: '3a3838',
+//     dark: '#34495e',
+//   },
+// })
+
+// export const originalTheme = Object.freeze({
+//   context: {
+//     'gradient': true,
+//     'shadow': 'lg', // 3 states: 'sm', 'lg', undefined (no shadow).
+//     'invertedColor': false,
+//   },
+//   colors: {
+//     primary: '#2b3635',
+//     secondary: '#65e097',
+//     success: '#40e583',
+//     info: '#2c82e0',
+//     danger: '#e34b4a',
+//     warning: '#ffc200',
+//     gray: '3a3838',
+//     dark: '#34495e',
+//   },
+// })
+
+export const corporateTheme = Object.freeze({
+  context: {
+    'gradient': false,
+    'shadow': 'sm',
+    'invertedColor': true,
+  },
+  colors: {
+    primary: '#6c7fee',
+    secondary: '#6e7ff1',
+    success: '#8ddc88',
+    info: '#71baff',
+    danger: '#f8706d',
+    warning: '#ffd652',
+    gray: '#8396a5',
+    dark: '#34495e',
+  },
+})
